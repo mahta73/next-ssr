2bd333fbed8c53a8c09c792dbaf88122fa64ae65
tekQueen <techandcodeaddict@gmail.com>
2018-09-22 10:11:56 +0400

    adding the first component to the pages folder

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+
+/*
+    All you need to do is just have components in your pages directory
+*/
+const Index = () => (
+    <div>
+        <h1>I LOVE COMPUTERS</h1>
+        <h2>I enjoy readig books and working with computers</h2>
+        <h3>I am building tons of skills at work</h3>
+    </div>
+);
+
+export default Index;
